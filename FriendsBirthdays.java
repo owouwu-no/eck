6678ae013dfcb90e000f47ee8092c24939ddf7ca
owouwu-no <54322027+owouwu-no@users.noreply.github.com>
2019-08-23 02:07:38 -0600

    Create FriendsBirthdays.java

---
+++ FriendsBirthdays.java
@@ -0,0 +1,10 @@
+public class FriendsBirthdays {
+    public static void main(String[] args){
+        System.out.println("Friend's Name   *   Friend's Birthday");
+        System.out.println("*************************************");
+        System.out.println(" Katie          *         8/25       ");
+        System.out.println(" Ian            *         3/22       ");
+        System.out.println(" Kassie         *         1/19       ");
+        System.out.println(" Lily           *         5/23       ");
+    }
+}
