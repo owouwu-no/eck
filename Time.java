6432c926446403dbf03baaa217f9e2a569a36970
owouwu-no <54322027+owouwu-no@users.noreply.github.com>
2019-08-26 09:05:48 -0600

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,6 @@
+public class Time {
+    // Make a variable that is the distance from home to work
+    // Make a variable that is your miles per gallon
+    // Make a variable for the price of a train ticket from home to work
+    //
+}
