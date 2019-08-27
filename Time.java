4ab2a9f2bdb95ddda1f145776a26c4ff75586a79
owouwu-no <54322027+owouwu-no@users.noreply.github.com>
2019-08-26 18:01:18 -0600

    Update Time.java

--- Time.java
+++ Time.java
@@ -2,5 +2,14 @@ public class Time {
     // Make a variable that is the distance from home to work
     // Make a variable that is your miles per gallon
     // Make a variable for the price of a train ticket from home to work
-    //
+    // Distance divided by miles per gallon = g
+    // f times 4 =cg
+    // distance times .05= cm
+    // cm + cg= price for car trip
+    // if price for car trip > price for ticket
+    //        tell user to use train ticket
+    // if price for car trap < price for ticket
+    //        tell user to take car
+    // if price for car trip = price for ticket
+    //         tell user to choose which they prefer
 }
