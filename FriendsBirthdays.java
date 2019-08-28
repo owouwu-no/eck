eb9c9bafdb6d1bfaafe620e5294d761e991a10a6
owouwu-no <54322027+owouwu-no@users.noreply.github.com>
2019-08-28 17:45:31 -0600

    Update FriendsBirthdays.java

--- FriendsBirthdays.java
+++ FriendsBirthdays.java
@@ -7,4 +7,11 @@ public class FriendsBirthdays {
         System.out.println(" Kassie         *         1/19       ");
         System.out.println(" Lily           *         5/23       ");
     }
+    // make a table using the print function and stars as the lines
+    // type "friend's name" as first header and "friend's birthday" as second header
+    // put 1 name and one birthday per line in the printed table
+
+    // Input: names, birthday, friend name header, friend birthday header, table format
+    // Process: compile text, print table
+    // Output: the table
 }
